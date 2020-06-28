a86789fd4fd7c72b302edce2a0266b0cd5e77143
Mithun Technologies <devopstrainingblr@gmail.com>
2020-06-28 13:23:33 +0530

    Initial Commit

---
+++ terraform_scripts/instances.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "kubernetes_server" {
+    count                   = 1
+    ami                     = var.kubernetes_ami
+    instance_type           = var.master_instance_type
+    vpc_security_groups_ids = [aws_security_group.kubernetes_sg.id]
+    subnet_id               = element(aws_subnet.kubernetes_subnets.*.id, count.index)
+    key_name                = var.key_name
+
+    tags = {
+        Name = "Kubernetes_Servers"
+        Type = "Kubernetes_Master"
+    }
+}
+
+resource "aws_instance" "kubernetes_workers" {
+    count                   = 2
+    ami                     = var.kubernetes_ami
+    instance_type           = var.worker_instance_type
+    vpc_security_groups_ids = [aws_security_group.kubernetes_sg.id]
+    subnet_id               = element(aws_subnet.kubernetes_subnets.*.id, count.index)
+    key_name                = var.key_name
+
+    tags = {
+        Name = "Kubernetes_Servers"
+        Type = "Kubernetes_Master"
+    }
+}
---
+++ terraform_scripts/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+    default = "ap-south-1"
+}
+
+variable "key_name" {
+    default = "Mumbaipem"
+}
+variable "vpc_cidr" {
+    default = "172.31.0.0/16"
+}
+variable "subnets_cidr" {
+    type = list{string}
+    default = ["172.31.0.0/16", "172.31.0.0/16"]
+}
+variable "availability_zones" {
+    type = list{string}
+    type = ["ap-south-1a", "ap-south-1b"]
+}
+variable "kubernetes_ami" {
+    default = "ami-02d55cb47e83a99a0"
+}
+variable = "master_instance_type" {
+    default = "t2.medium"
+}
+variable = "worker_instance_type" {
+    default = "t2.micro"
+}
